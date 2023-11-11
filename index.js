ca8e4a445269f66de42e645cc1cc0fb5237026cc
dsleazii <dsleazii@gmail.com>
2023-11-11 05:00:25 -0500

    added forecast function

--- index.js
+++ index.js
@@ -1,66 +1,101 @@
-const API = "0baf0dab3ca4e1359t8bb81651943o3d";
-const apiUrl = "https://api.shecodes.io/weather/v1/forecast?units=metric&q=";
+const apiKey = "0baf0dab3ca4e1359t8bb81651943o3d";
+const apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&untis=metric";
 
-const searchformElement = document.querySelector("#searchBox");
-const searchformElement = document.querySelector("#search-form-input");
 const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
-  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-  var data = await response.json();
+  const response = await fetch(
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&untis=metric" +
+      city +
+      `&key=0baf0dab3ca4e1359t8bb81651943o3d`
+  );
+  var data = await response.json(); 
 
-  console.log(data);
+  console.log(data);};
 
-  document.querySelector(".city").innerHTML = data.name;
+  {document.querySelector(".city").innerHTML = data.city;
   document.querySelector(".temp").innerHTML =
-    Mathround(data.main.temp) + "&degF";
-  document.querySelector(".humdity").innerHTML = data.main.humidity + "%";
+    Math.round(data.temperature.current) + "&deg;F";
+  document.querySelector(".humidity").innerHTML =
+    data.temperature.humidity + "%";
   document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+  if (data.condition.description == " Day Clouds") {
+    weatherIcon.src = "Pictures/xln6kdfh.png";
+  } else if (data.condition.description == "Clear Sunny") {
+    weatherIcon.src = "Pictures/rgxs1sb9.png";
+  } else if (data.condition.description == "Clear Dark") {
+    weatherIcon.src = "Pictures/v9q2wq60.png";
+  } else if (data.condition.description == "Day Wind") {
+    weatherIcon.src = "Pictures/y2ctkjdq.png";
+  } else if (data.condition.description == "Night Wind") {
+    weatherIcon.src = "Pictures/zaf6lqpb.png";
+  } else if (data.condition.description == "Day Snow") {
+    weatherIcon.src = "Pictures/kp4gh57w.png";
+  } else if (data.condition.description == "Night Snow") {
+    weatherIcon.src = "Pictures/6q8aiq0i.png";
+  } else if (data.condition.description == "Day Thunderstorm") {
+    weatherIcon.src = "Pictures/1jv93qmg.png";
+  } else if (data.condition.description == "Night Thunderstorm") {
+    weatherIcon.src = "Pictures/8kjxong.png";
+  } else if (data.condition.description == "Day Rain") {
+    weatherIcon.src = "Pictures/t1v2memj.png";
+  } else if (data.condition.description == "Day Sun Rain") {
+    weatherIcon.src = "Pictures/qu5pgg7v.png";
+  } else if (data.condition.description == "Night Rain") {
+    weatherIcon.src = "Pictures/bkm5glwo.png";
+  } else if (data.condition.description == "Night Sun Rain") {
+    weatherIcon.src = "Pictures/qrd3vgpm.png";
+  } else if (data.condition.description == "Day cloudy") {
+    weatherIcon.src = "Pictures/bunjrakw.png";
+  } else if (data.condition.description == "Night cloudy") {
+    weatherIcon.src = "Pictures/84irhhgq.png";
+  }
 }
-
-if (data.weather[0].main == " Day Clouds") {
-  weatherIcon.src = "Pictures/xln6kdfh.png";
-} else if (data.weather[0].main == "Clear Sunny") {
-  weatherIcon.src = "Pictures/rgxs1sb9.png";
-} else if (data.weather[0].main == "Clear Dark") {
-  weatherIcon.src = "Pictures/v9q2wq60.png";
-} else if (data.weather[0].main == "Day Wind") {
-  weatherIcon.src = "Pictures/y2ctkjdq.png";
-} else if (data.weather[0].main == "Night Wind") {
-  weatherIcon.src = "Pictures/zaf6lqpb.png";
-} else if (data.weather[0].main == "Day Snow") {
-  weatherIcon.src = "Pictures/kp4gh57w.png";
-} else if (data.weather[0].main == "Night Snow") {
-  weatherIcon.src = "Pictures/6q8aiq0i.png";
-} else if (data.weather[0].main == "Day Thunderstorm") {
-  weatherIcon.src = "Pictures/1jv93qmg.png";
-} else if (data.weather[0].main == "Night Thunderstorm") {
-  weatherIcon.src = "Pictures/8kjxong.png";
-} else if (data.weather[0].main == "Day Rain") {
-  weatherIcon.src = "Pictures/t1v2memj.png";
-} else if (data.weather[0].main == "Day Sun Rain") {
-  weatherIcon.src = "Pictures/qu5pgg7v.png";
-} else if (data.weather[0].main == "Night Rain") {
-  weatherIcon.src = "Pictures/bkm5glwo.png";
-} else if (data.weather[0].main == "Night Sun Rain") {
-  weatherIcon.src = "Pictures/qrd3vgpm.png";
-} else if (data.weather[0].main == "Day cloudy") {
-  weatherIcon.src = "Pictures/bunjrakw.png";
-} else if (data.weather[0].main == "Night cloudy") {
-  weatherIcon.src = "Pictures/84irhhgq.png";
-}
-
-document.querySelector(".weather").computedStyleMap.display = block;
-
-searchBTN.addEventListener("click", () => {
-  checkWeather(searchBox.value);
-});
 
 function handleSearchSubmit(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("#search-from-input");
+  let searchInput = document.querySelector("#search-form-input");
   console.log(searchInput.value);
+  checkWeather(searchInput.value);
 }
 
-let searchfromElement = document.querySelector("#search-form");
+function getForecast(city){
+  let apiKey = "0baf0dab3ca4e1359t8bb81651943o3d";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric";
+    console.log(apiUrl);
+}
+
+function displayForeCast() {
+
+
+let days = ["Tues", "Wed", "Thrus", "Fri", "Sat"];
+let forecastHTML = "";
+
+days.forEach(function (day) {
+forecastHTML =
+  forecastHTML +
+  ` <div class="list_content" id="forecast">
+            <div class= "weather-forecast-date" > ${day}</div>
+            <div class= "weather-forecast-icon" img src= "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-night.png" >🌤️</div>
+            <div class= "weather-forecast-temperatures" ></div>
+            <div class= "weather-forecast-temperature" >
+              <strong>23&deg;F</strong>
+            </div>
+            
+              
+      
+
+`; }); 
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml;
+}
+
+let searchformElement = document.querySelector("#search-form");
 searchformElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("France");
+getForecast("France");
+displayForeCast();
